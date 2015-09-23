4158002ee4e0fffecc0bb880e97c46347dcc4495
Educacion.be <guzmanhilario2009@hotmail.com>
2015-09-23 13:23:27 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,15 +1,11 @@
 <?php
-
 /*
  * @author    Shaun Daubney
  * @package   theme_educacionbe
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die;
-
 if ($ADMIN->fulltree) {
-
     // Basic Heading
     $name = 'theme_educacionbe/basicheading';
     $heading = get_string('basicheading', 'theme_educacionbe');
@@ -23,16 +19,14 @@ if ($ADMIN->fulltree) {
 	$description = get_string('logodesc', 'theme_educacionbe');
 	$setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
 	$settings->add($setting);	
-
 	// Hide Menu
-	$name = 'theme_aardvark/hidemenu';
+	$name = 'theme_educacionbe/hidemenu';
 	$title = get_string('hidemenu','theme_educacionbe');
 	$description = get_string('hidemenudesc', 'theme_educacionbe');
 	$default = 1;
 	$choices = array(1=>get_string('yes',''), 0=>get_string('no',''));
 	$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
 	$settings->add($setting);
-
 	// Email url setting
 	$name = 'theme_educacionbe/emailurl';
 	$title = get_string('emailurl','theme_educacionbe');
@@ -40,7 +34,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Custom CSS file
 	$name = 'theme_educacionbe/customcss';
 	$title = get_string('customcss','theme_educacionbe');
@@ -49,14 +42,12 @@ if ($ADMIN->fulltree) {
 	$setting = new admin_setting_configtextarea($name, $title, $description, $default);
 	$setting->set_updatedcallback('theme_reset_all_caches');
 	$settings->add($setting);
-
 	// Frontpage Heading
     $name = 'theme_educacionbe/frontpageheading';
     $heading = get_string('frontpageheading', 'theme_educacionbe');
     $information = get_string('frontpageheadingdesc', 'theme_educacionbe');
     $setting = new admin_setting_heading($name, $heading, $information);
     $settings->add($setting);
-
 	// Title Date setting
 	$name = 'theme_educacionbe/titledate';
 	$title = get_string('titledate','theme_educacionbe');
@@ -65,7 +56,6 @@ if ($ADMIN->fulltree) {
 	$choices = array(1=>get_string('yes',''), 0=>get_string('no',''));
 	$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
 	$settings->add($setting);
-
 	// General Alert setting
 	$name = 'theme_educacionbe/generalalert';
 	$title = get_string('generalalert','theme_educacionbe');
@@ -73,7 +63,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Snow Alert setting
 	$name = 'theme_educacionbe/snowalert';
 	$title = get_string('snowalert','theme_educacionbe');
@@ -81,7 +70,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
     // Colour Heading
     $name = 'theme_educacionbe/colourheading';
     $heading = get_string('colourheading', 'theme_educacionbe');
@@ -97,7 +85,6 @@ if ($ADMIN->fulltree) {
 	$previewconfig = NULL;
 	$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
 	$settings->add($setting);
-
 	// Graphic Wrap (Background Image)
 	$name = 'theme_educacionbe/backimage';
 	$title=get_string('backimage','theme_educacionbe');
@@ -105,7 +92,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
 	$settings->add($setting);
-
 	// Graphic Wrap (Background Position)
 	$name = 'theme_educacionbe/backposition';
 	$title = get_string('backposition','theme_educacionbe');
@@ -114,7 +100,6 @@ if ($ADMIN->fulltree) {
 	$choices = array('no-repeat'=>get_string('backpositioncentred','theme_educacionbe'), 'no-repeat fixed'=>get_string('backpositionfixed','theme_educacionbe'), 'repeat'=>get_string('backpositiontiled','theme_educacionbe'), 'repeat-x'=>get_string('backpositionrepeat','theme_educacionbe'));
 	$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
 	$settings->add($setting);
-
 	// Menu hover background colour setting
 	$name = 'theme_educacionbe/menuhovercolor';
 	$title = get_string('menuhovercolor','theme_educacionbe');
@@ -138,7 +123,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// CEOP
 	$name = 'theme_educacionbe/ceop';
 	$title = get_string('ceop','theme_educacionbe');
@@ -147,7 +131,6 @@ if ($ADMIN->fulltree) {
 	$choices = array(''=>get_string('ceopnone','theme_educacionbe'), 'http://www.thinkuknow.org.au/site/report.asp'=>get_string('ceopaus','theme_educacionbe'), 'http://www.ceop.police.uk/report-abuse/'=>get_string('ceopuk','theme_educacionbe'));
 	$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
 	$settings->add($setting);
-
 	// Disclaimer setting
 	$name = 'theme_educacionbe/disclaimer';
 	$title = get_string('disclaimer','theme_educacionbe');
@@ -155,7 +138,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
 	$settings->add($setting);	
-
 	// Social Icons Heading
     $name = 'theme_educacionbe/socialiconsheading';
     $heading = get_string('socialiconsheading', 'theme_educacionbe');
@@ -170,7 +152,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Facebook url setting
 	$name = 'theme_educacionbe/facebook';
 	$title = get_string('facebook','theme_educacionbe');
@@ -178,15 +159,13 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Twitter url setting
-	$name = 'theme_aardvark/twitter';
+	$name = 'theme_educacionbe/twitter';
 	$title = get_string('twitter','theme_educacionbe');
 	$description = get_string('twitterdesc', 'theme_educacionbe');
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Google+ url setting
 	$name = 'theme_educacionbe/googleplus';
 	$title = get_string('googleplus','theme_educacionbe');
@@ -194,7 +173,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Flickr url setting
 	$name = 'theme_educacionbe/flickr';
 	$title = get_string('flickr','theme_educacionbe');
@@ -202,7 +180,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Pinterest url setting
 	$name = 'theme_educacionbe/pinterest';
 	$title = get_string('pinterest','theme_educacionbe');
@@ -210,7 +187,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Instagram url setting
 	$name = 'theme_educacionbe/instagram';
 	$title = get_string('instagram','theme_educacionbe');
@@ -218,7 +194,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// LinkedIn url setting
 	$name = 'theme_educacionbe/linkedin';
 	$title = get_string('linkedin','theme_educacionbe');
@@ -234,7 +209,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// YouTube url setting
 	$name = 'theme_educacionbe/youtube';
 	$title = get_string('youtube','theme_educacionbe');
@@ -242,7 +216,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Apple url setting
 	$name = 'theme_educacionbe/apple';
 	$title = get_string('apple','theme_educacionbe');
@@ -250,7 +223,6 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 	// Android url setting
 	$name = 'theme_educacionbe/android';
 	$title = get_string('android','theme_educacionbe');
@@ -258,6 +230,4 @@ if ($ADMIN->fulltree) {
 	$default = '';
 	$setting = new admin_setting_configtext($name, $title, $description, $default);
 	$settings->add($setting);
-
 }
-
